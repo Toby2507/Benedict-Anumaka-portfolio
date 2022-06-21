5549bc09ba5fb8f07910b6fd1249574db9486cf8
Toby2507 <tswagsurboi@gmail.com>
2022-06-21 18:25:30 +0100

    Added new Project

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,65 +18,60 @@ ham.addEventListener("click", function () {
 const presentYear = new Date().getFullYear();
 year.innerText = presentYear;
 // Typewriter function
-function Typewriter(txtElement, words, wait) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = "";
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
-// Type Method
-Typewriter.prototype.type = function () {
-    const i = this.wordIndex % this.words.length;
-    const fullTxt = this.words[i];
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-    // insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-    // type speed
-    let typeSpeed = 200;
-    if (this.isDeleting) {
-        typeSpeed /= 2;
-    }
-    if (!this.isDeleting && this.txt == fullTxt) {
-        typeSpeed = this.wait;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt == "") {
+function showTypewriter() {
+    function Typewriter(txtElement, words, wait) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = "";
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
         this.isDeleting = false;
-        this.wordIndex++;
-        typeSpeed = 400;
     }
-    // Timeout
-    setTimeout(() => this.type(), typeSpeed)
-}
-// Init on DOM Load
-window.addEventListener("DOMContentLoaded", init);
-// Init app
-function init() {
-    const txtElement = document.querySelector("#roles");
-    const words = JSON.parse(txtElement.dataset.words);
-    const wait = txtElement.dataset.wait;
-    // init typewriter
-    new Typewriter(txtElement, words, wait);
-}
-// form functionality
-const inputs = document.querySelectorAll(".inputs");
+    // Type Method
+    Typewriter.prototype.type = function () {
+        const i = this.wordIndex % this.words.length;
+        const fullTxt = this.words[i];
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        // insert txt into element
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-window.addEventListener("load", function () {
-    inputs.forEach(function (input) {
-        input.value = "";
+        // type speed
+        let typeSpeed = 200;
+        if (this.isDeleting) {
+            typeSpeed /= 2;
+        }
+        if (!this.isDeleting && this.txt == fullTxt) {
+            typeSpeed = this.wait;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt == "") {
+            this.isDeleting = false;
+            this.wordIndex++;
+            typeSpeed = 400;
+        }
+        // Timeout
+        setTimeout(() => this.type(), typeSpeed)
+    }
+    // Init on DOM Load
+    window.addEventListener("DOMContentLoaded", init);
+    // Init app
+    function init() {
+        const txtElement = document.querySelector("#roles");
+        const words = JSON.parse(txtElement.dataset.words);
+        const wait = txtElement.dataset.wait;
+        // init typewriter
+        new Typewriter(txtElement, words, wait);
+    }
+    // form functionality
+    const inputs = document.querySelectorAll(".inputs");
+
+    window.addEventListener("load", function () {
+        inputs.forEach(function (input) {
+            input.value = "";
+        })
     })
-})
-
-
-
-
-
-
-
+}
